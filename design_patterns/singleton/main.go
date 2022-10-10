e17adbfecd296633f4ba20fc87a9aa6049f7b773
famousmai <906631095@qq.com>
2022-10-10 20:11:45 +0800

    feat(design patterns): another example syncOnce for singleton

--- design_patterns/singleton/main.go
+++ design_patterns/singleton/main.go
@@ -7,9 +7,12 @@ import (
 
 var lock = &sync.Mutex{}
 
+var once sync.Once
+
 type single struct{}
 
 var singleInstance *single
+var singleInstance1 *single
 
 /*
 *
@@ -36,6 +39,30 @@ func getInstance() *single {
 	return singleInstance
 }
 
+/*
+*
+1. init函数
+我们可以在 init函数中创建单例实例。
+这仅适用于实例的早期初始化工作已经确定时。
+init函数仅会在包中的每个文件里调用一次， 所以我们可以确定其只会创建一个实例。
+
+2. sync.Once
+sync.Once仅会执行一次操作。 可查看下面的代码：
+*/
+func getInstanceUseSyncOnce(i int) *single {
+	if singleInstance1 == nil {
+		fmt.Println(i)
+		once.Do(
+			func() {
+				fmt.Println("Creating1 single instance now.")
+				singleInstance1 = &single{}
+			})
+	} else {
+		fmt.Println("Single instance1 already created.")
+	}
+	return singleInstance1
+}
+
 func main() {
 	var waitGroup sync.WaitGroup
 
@@ -49,4 +76,17 @@ func main() {
 	}
 
 	waitGroup.Wait()
+
+	var waitGroup2 sync.WaitGroup
+	for i := 0; i < 10; i++ {
+
+		waitGroup2.Add(1)
+		go func() {
+			getInstanceUseSyncOnce(i)
+			defer waitGroup2.Done()
+
+		}()
+	}
+
+	waitGroup2.Wait()
 }
